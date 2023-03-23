cea1790e2665cbd9456a3a817bab4ab82aeb549d
Z333Q <127899521+Z333Q@users.noreply.github.com>
2023-03-23 17:06:38 +0400

    Update HazelHearts.sol

--- contracts/HazelHearts.sol
+++ contracts/HazelHearts.sol
@@ -1,50 +1,101 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.9;
+pragma solidity 0.8.19;
 
-import "./HazelHeartsToken.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract HazelHearts {
-    uint public constant MAX_FARMS = 10000;
-    uint public constant MAX_FREE_MINTS = 200;
+contract HazelHearts is Ownable {
+using SafeMath for uint256;
+address payable public daoTreasury;
 
-    address public owner;
-    uint public numMinted;
-    HazelHeartsToken public hazelHeartsToken;
+uint256 public tierOnePrice;
+uint256 public tierTwoPrice;
+uint256 public tierThreePrice;
+uint256 public tierFourPrice;
+uint256 public tierFivePrice;
 
-    constructor(address _hazelHeartsTokenAddress) {
-        owner = msg.sender;
-        hazelHeartsToken = HazelHeartsToken(_hazelHeartsTokenAddress);
-    }
+uint256 public constant maxSupply = 1000;
 
-    function mint(address to) external {
-        require(numMinted < MAX_FARMS, "All farms have been minted");
-        if (numMinted < MAX_FREE_MINTS) {
-            hazelHeartsToken.mint(to, 5);
-        } else if (numMinted < MAX_FREE_MINTS + 200) {
-            require(msg.value == 300000000000000000, "Payment amount is incorrect");
-            hazelHeartsToken.mint(to, 10);
-        } else if (numMinted < MAX_FREE_MINTS + 400) {
-            require(msg.value == 500000000000000000, "Payment amount is incorrect");
-            hazelHeartsToken.mint(to, 15);
-        } else if (numMinted < MAX_FREE_MINTS + 600) {
-            require(msg.value == 1000000000000000000, "Payment amount is incorrect");
-            hazelHeartsToken.mint(to, 25);
-        } else {
-            require(msg.value == 2000000000000000000, "Payment amount is incorrect");
-            hazelHeartsToken.mint(to, 50);
-        }
-        numMinted++;
-    }
+IERC721 public farmContract;
+address public hazelHeartsGame;
 
-    function getTokens() external {
-        hazelHeartsToken.transfer(msg.sender, 10);
-    }
-
-    function withdraw() external {
-        require(msg.sender == owner, "Only the owner can withdraw");
-        payable(owner).transfer(address(this).balance);
-    }
-
-    receive() external payable {}
+constructor(
+    address payable _daoTreasury,
+    uint256 _tierOnePrice,
+    uint256 _tierTwoPrice,
+    uint256 _tierThreePrice,
+    uint256 _tierFourPrice,
+    uint256 _tierFivePrice,
+    address _farmContract,
+    address _hazelHeartsGame
+) {
+    daoTreasury = _daoTreasury;
+    tierOnePrice = _tierOnePrice;
+    tierTwoPrice = _tierTwoPrice;
+    tierThreePrice = _tierThreePrice;
+    tierFourPrice = _tierFourPrice;
+    tierFivePrice = _tierFivePrice;
+    farmContract = IERC721(_farmContract);
+    hazelHeartsGame = _hazelHeartsGame;
 }
+
+function mint(address to, uint256 tokenId) external payable {
+    require(tokenId < maxSupply, "Token ID out of range");
+    require(msg.value >= getCurrentPrice(tokenId), "Ether value sent is not correct");
+
+    if (tokenId >= 1 && tokenId <= 200) {
+        farmContract.createFarm(to);
+        HazelHeartsGame(hazelHeartsGame).mintHazelHearts(to, tokenId, 5);
+    } else if (tokenId >= 201 && tokenId <= 400) {
+        HazelHeartsGame(hazelHeartsGame).mintHazelHearts(to, tokenId, 10);
+    } else if (tokenId >= 401 && tokenId <= 600) {
+        HazelHeartsGame(hazelHeartsGame).mintHazelHearts(to, tokenId, 15);
+    } else if (tokenId >= 601 && tokenId <= 800) {
+        HazelHeartsGame(hazelHeartsGame).mintHazelHearts(to, tokenId, 25);
+    } else if (tokenId >= 801 && tokenId <= 1000) {
+        HazelHeartsGame(hazelHeartsGame).mintHazelHearts(to, tokenId, 50);
+    }
+
+    daoTreasury.transfer(msg.value);
+}
+
+function getCurrentPrice(uint256 tokenId) public view returns (uint256) {
+    if (tokenId >= 1 && tokenId <= 200) {
+        return tierOnePrice;
+    } else if (tokenId >= 201 && tokenId <= 400) {
+        return tierTwoPrice;
+    } else if (tokenId >= 401 && tokenId <= 600) {
+        return tierThreePrice;
+       } else if (tokenId >= 601 && tokenId <= 800) {
+        return tierFourPrice;
+    } else if (tokenId >= 801 && tokenId <= 1000) {
+        return tierFivePrice;
+    }
+}
+
+function setTierOnePrice(uint256 _price) external onlyOwner {
+    tierOnePrice = _price;
+}
+
+function setTierTwoPrice(uint256 _price) external onlyOwner {
+    tierTwoPrice = _price;
+}
+
+function setTierThreePrice(uint256 _price) external onlyOwner {
+    tierThreePrice = _price;
+}
+
+function setTierFourPrice(uint256 _price) external onlyOwner {
+    tierFourPrice = _price;
+}
+
+function setTierFivePrice(uint256 _price) external onlyOwner {
+    tierFivePrice = _price;
+}
+
+function setDaoTreasury(address payable _daoTreasury) external onlyOwner {
+    daoTreasury = _daoTreasury;
+}
+
